40c93802d17a58e6c82008c4e19066a3e7ad46fb
Barbara <barbara@git>
2023-02-09 16:19:53 +0100

    trying another config for vercel

--- index.js
+++ index.js
@@ -32,6 +32,3 @@ app.use('/api', userRouter, seekerRouter, recruiterRouter)
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`)
 })
-
-// Export the Express API
-module.exports = app
